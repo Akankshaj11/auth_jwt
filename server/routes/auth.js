2f0dfef655ccded6aa32790a723b519170ec06ea
Akankshaj11 <akankshajedhe96@gmail.com>
2025-08-24 18:14:17 +0530

    modified files

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -30,31 +30,6 @@ router.post("/register", async (req, res) => {
   }
 });
 
-// login router
-
-// router.post("/login", async( req, res) => {
-//   const { email, password } = req.body;
-
-//   try{
-//     const user = await User.findOne({ email });
-//     if(!user){
-//       return res.status(400).json({ message: "Invalid email or password" });
-//     }
-
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     if(!isMatch){
-//       return res.status(400).json({ message: "Invalid email or password" });
-//     }
-
-//     const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: '1h'});
-//     res.status(200).json({ message: "Login successful", token });
-//     res.json({ token });
-
-//   } catch(error){
-//     res.status(500).json({ message: "Server error", error: error.message });
-//   }
-// })
-
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
